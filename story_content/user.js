fa90cc1296eb17ce32113df05ff81d47d214333c
grretopot-art <grretopot@gmail.com>
2026-04-07 22:34:11 +0530

    Add files via upload

---
+++ story_content/user.js
@@ -0,0 +1,44 @@
+window.InitUserScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+window.Script1 = function()
+{
+  //load the scripts dynamically into the head of the document
+function add_line() {
+ var line = document.createElement("audio");
+ var head=document.getElementsByTagName('body')[0];
+ line.type = "audio/mp3";
+ line.src="";
+ line.id="bgSong" ;
+ line.autoplay = true;
+ line.loop = true;
+ head.appendChild(line);
+}
+//but we only want to add these once!
+if(document.getElementById('bgSong')==null){
+ add_line();
+var audio = document.getElementById('bgSong');
+audio.volume = 0.4;
+}
+var  player=GetPlayer();
+this.Location=player.GetVar("location");
+
+var audio=document.getElementById('bgSong');
+audio.src=Location+"bgmusic_hept_low.mp3";
+audio.load();
+audio.play();
+}
+
+};
